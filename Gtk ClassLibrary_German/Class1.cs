8c60b7ef520e8f369842fec2fd51fb5c424f98ba
cfwprpht <darknight3111@gmx.at>
2017-03-31 06:30:39 +0200

    Initial Release
    A Visual Studio C# Gtk Class Template.

---
+++ Gtk ClassLibrary_German/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+$if$ ($targetframeworkversion$ >= 4.5)using System.Threading.Tasks;
+$endif$
+using Gtk;
+
+namespace $safeprojectname$
+{
+    public class Class1
+    {
+    }
+}
